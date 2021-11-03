07728dad96166b32bd557d295b9d1185bf9d0d3c
Square-Lion <npb630@verizon.net>
2021-11-03 11:48:05 -0400

    Got Rid Of Hamnav

--- main.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu(){
-    let getMenu = document.querySelector(".mainMenu");
-    getMenu.classList.toggle("hamburger");
-}
-let getHamburger = document.querySelector("#toggle-bar");
-getHamburger.addEventListener("click",toggleMenu);

77368f10c617086c6b3141238f47715e1aabdebc
annemarieakoh <91986153+annemarieakoh@users.noreply.github.com>
2021-12-03 16:22:08 -0500

    Update purchase.js

--- TEAM07_FINALWEBSITE/Team07/js/purchase.js
+++ TEAM07_FINALWEBSITE/Team07/js/purchase.js
@@ -1,9 +1,9 @@
 function showGroceryBagsPrice() {
-  var title = document.getElementById("grocerybags");
-  if (title.innerHTML == "Show Price) {
+  var title = document.getElementByTagName("figcaption");
+  if (title.innerHTML == "Price:") {
     title.innerHTML = 'Currently $26.99';
   } 
   else {
-    title.innerHTML = "Show Price";
+    title.innerHTML = "Price:";
   }
 }

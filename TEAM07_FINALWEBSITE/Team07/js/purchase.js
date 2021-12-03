be32a3762c9aba1020c21c106c0bd21a72f629c6
annemarieakoh <91986153+annemarieakoh@users.noreply.github.com>
2021-12-03 16:44:42 -0500

    Update purchase.js

--- TEAM07_FINALWEBSITE/Team07/js/purchase.js
+++ TEAM07_FINALWEBSITE/Team07/js/purchase.js
@@ -1,9 +1,89 @@
-function showGroceryBagsPrice() {
-  var title = document.getElementById("caption");
-  if (title.innerHTML == "Price:") {
-    title.innerHTML = 'Price: Currently $26.99';
+function toggleGroceryBagsPrice() {
+  var cap = document.getElementById("caption");
+  if (cap.innerHTML == "Price:") {
+    cap.innerHTML = 'Price: Currently $26.99';
   } 
   else {
-    title.innerHTML = "Price:";
+    cap.innerHTML = "Price:";
+  }
+}
+
+function toggleLanternsPrice() {
+  var cap = document.getElementById("caption");
+  if (cap.innerHTML == "Price:") {
+    cap.innerHTML = 'Price: Currently $28.00';
+  } 
+  else {
+    cap.innerHTML = "Price:";
+  }
+}
+
+function toggleLightbulbsPrice() {
+  var cap = document.getElementById("caption");
+  if (cap.innerHTML == "Price:") {
+    cap.innerHTML = 'Price: Currently $12.78';
+  } 
+  else {
+    cap.innerHTML = "Price:";
+  }
+}
+
+function toggleWalletPrice() {
+  var cap = document.getElementById("caption");
+  if (cap.innerHTML == "Price:") {
+    cap.innerHTML = 'Price: Currently $25.00';
+  } 
+  else {
+    cap.innerHTML = "Price:";
+  }
+}
+
+function toggleHandbagPrice() {
+  var cap = document.getElementById("caption");
+  if (cap.innerHTML == "Price:") {
+    cap.innerHTML = 'Price: Currently $70.00-$80.00';
+  } 
+  else {
+    cap.innerHTML = "Price:";
+  }
+}
+
+function togglePhoneCasePrice() {
+  var cap = document.getElementById("caption");
+  if (cap.innerHTML == "Price:") {
+    cap.innerHTML = 'Price: Currently $49.95-$54.95';
+  } 
+  else {
+    cap.innerHTML = "Price:";
+  }
+}
+
+function toggleCoffeeCupPrice() {
+  var cap = document.getElementById("caption");
+  if (cap.innerHTML == "Price:") {
+    cap.innerHTML = 'Price: Currently $19.95';
+  } 
+  else {
+    cap.innerHTML = "Price:";
+  }
+}
+
+function toggleFlashlightPrice() {
+  var cap = document.getElementById("caption");
+  if (cap.innerHTML == "Price:") {
+    cap.innerHTML = 'Price: Currently $63.65';
+  } 
+  else {
+    cap.innerHTML = "Price:";
+  }
+}
+
+function toggleCrockeryPrice() {
+  var cap = document.getElementById("caption");
+  if (cap.innerHTML == "Price:") {
+    cap.innerHTML = 'Price: Currently $28.00';
+  } 
+  else {
+    cap.innerHTML = "Price:";
   }
 }

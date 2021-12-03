c821ad60a22b14784d897abd230b92c79ccfc890
annemarieakoh <91986153+annemarieakoh@users.noreply.github.com>
2021-12-03 15:51:13 -0500

    Update purchase.js

--- TEAM07_FINALWEBSITE/Team07/js/purchase.js
+++ TEAM07_FINALWEBSITE/Team07/js/purchase.js
@@ -1,1 +1,9 @@
-
+function showGroceryBagsPrice() {
+  var img = document.getElementById("grocerybags");
+  var title = document.getElementById("GroceryBagsTitle");
+  if (title.innerHTML === "Reusable Grocery Bags") {
+    title.innerHTML = "Currently $26.99";
+  } else {
+    title.innerHTML = "Reusable Grocery Bags";
+  }
+}

ac4aca737cdbdc37f60110f536c7765a035a0fda
Drew Donohue <drewdonohue13@gmail.com>
2021-12-02 22:36:24 -0500

    added chat log

--- TEAM07_FINALWEBSITE/Team07/js/speadtheword.js
+++ TEAM07_FINALWEBSITE/Team07/js/speadtheword.js
@@ -1,4 +1,28 @@
-function Post(inputbox){
-	var username = document.getElementByID("username");
-	var board = document.getElementByID("board");
+var username;
+
+function PostUsername()
+{
+    var button = document.getElementById("buttonRemove");
+    button.remove();
+    
+    var box = document.getElementById("username");
+    var actualName = box.value;
+    box.remove();
+    
+    var usernameReplace = document.getElementById("replaceName");
+    
+    usernameReplace.innerHTML = actualName;
+    username = actualName;   
+}
+
+
+function Post()
+{
+    var username = globalThis.username;
+    var getBoard = document.getElementById("post");
+    var insertText = getBoard.value;
+    
+    var board = document.getElementById("chat");
+    var text = (username + ":" + insertText + "<br>");
+    board.innerHTML += text
 }
